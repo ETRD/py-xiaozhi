f778ecc70b46383779e0d719e53241a28c4c1bdd
launcher <fpga@live.cn>
2026-03-21 09:40:13 +0800

    modify for hw

--- src/views/activation/__init__.py
+++ src/views/activation/__init__.py
@@ -1,4 +1,6 @@
 # 设备激活界面包
-from .activation_window import ActivationWindow
+#from .activation_window import ActivationWindow
 
-__all__ = ["ActivationWindow"]
+#__all__ = ["ActivationWindow"]
+
+__all__ = ["ActivationWindow", "CLIActivation"]
